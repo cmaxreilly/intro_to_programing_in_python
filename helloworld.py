e95a56795050858ef0321e147bbc33abc8606c13
cmaxreilly <c.maxwell.reilly@gmail.com>
2022-10-17 10:02:26 -0600

    Hello world, usrargument

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+
+
+# This version did not work. stdio doesn't have a writeln attribute. I wonder if it was deprecated?
+import stdio
+
+stdio.write('Hello, World!')
+
+# Actually, looks like its just a library someone wrote. Going to try to figure out how to import it.
+
+# Aha! figured it out. It's a book library. Don't know why I'm using it, lol. but here we are!
+
---
+++ useargument.py
@@ -0,0 +1,8 @@
+import stdio
+import sys
+
+# Accept a name as a commnad-line argument. Write a message containing that name to standard output.
+
+stdio.write('Hi, ')
+stdio.write(sys.argv[1])
+stdio.writeln('. How are you?')
